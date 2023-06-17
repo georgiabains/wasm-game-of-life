265d1832c67afcf2e97d0fb84ff50ac7e265ddfd
georgiabains <georgiabains@hotmail.co.uk>
2023-06-17 17:30:04 +0100

    Game in playable state with play/pause button

---
+++ www/webpack.config.js
@@ -0,0 +1,34 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "index.html",
+        },
+      ],
+    }),
+  ],
+  experiments: {
+    syncWebAssembly: true, // deprecated, see https://github.com/webpack/webpack/issues/11347
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
