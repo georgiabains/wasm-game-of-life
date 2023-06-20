88e43cee9579befc9746fcf8bc40317215ecf85f
georgiabains <georgiabains@hotmail.co.uk>
2023-06-20 23:17:24 +0100

    Attempt to fix public path

--- www/webpack.prod.js
+++ www/webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: "production",
   devtool: 'source-map',
   output: {
-    publicPath: './www/dist/',
+    publicPath: './',
   },
   experiments: {
     syncWebAssembly: true, // deprecated, see https://github.com/webpack/webpack/issues/11347
